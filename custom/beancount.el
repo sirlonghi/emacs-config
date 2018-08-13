74be1e1e907d39971d80a491338731fbc0fdb11c
Francesco Longhi <longhi.francesco@gmail.com>
2018-08-13 11:52:12 +0200

    Initial commit

---
+++ custom/beancount.el
@@ -0,0 +1,365 @@
+;;; beancount.el --- A minor mode that can be used to edit beancount input files.
+
+;; Copyright (C) 2013 Martin Blais <blais@furius.ca>
+
+;; This file is not part of GNU Emacs.
+
+(require 'ido) ;; For completing read.
+(require 'font-lock)
+
+
+(define-minor-mode beancount-mode
+  "A minor mode to help editing Beancount files.
+This can be used within other text modes, in particular, org-mode
+is great for sectioning large files with many transactions."
+  :init-value nil
+  :lighter " Beancount"
+  :keymap
+  '(
+    ([(control c)(\')] . beancount-insert-account)
+    ([(control c)(control g)] . beancount-transaction-set-flag)
+    ([(control c)(r)] . beancount-init-accounts)
+    ([(control c)(l)] . beancount-check)
+    ([(control c)(q)] . beancount-query)
+    ([(control c)(x)] . beancount-context)
+    ([(control c)(\;)] . beancount-align-to-previous-number)
+    ([(control c)(\:)] . beancount-align-numbers)
+    ([(control c)(p)] . beancount-test-align)
+    )
+  :group 'beancount
+
+  ;; The following is mostly lifted from lisp-mode.
+  (make-local-variable 'paragraph-ignore-fill-prefix)
+  (setq paragraph-ignore-fill-prefix t)
+  (make-local-variable 'fill-paragraph-function)
+  (setq fill-paragraph-function 'lisp-fill-paragraph)
+
+  (make-local-variable 'comment-start)
+  (setq comment-start ";; ")
+  (make-local-variable 'comment-start-skip)
+
+  ;; Look within the line for a ; following an even number of backslashes
+  ;; after either a non-backslash or the line beginning.
+  (setq comment-start-skip "\\(\\(^\\|[^\\\\\n]\\)\\(\\\\\\\\\\)*\\);+ *")
+  (make-local-variable 'font-lock-comment-start-skip)
+  ;; Font lock mode uses this only when it KNOWS a comment is starting.
+  (setq font-lock-comment-start-skip ";+ *")
+  (make-local-variable 'comment-add)
+  (setq comment-add 1) ;; Default to `;;' in comment-region
+
+  ;; No tabs by default.
+  (set (make-local-variable 'indent-tabs-mode) nil)
+
+  ;; Customize font-lock for beancount.
+  ;;
+  ;; Important: you have to use 'nil for the mode here because in certain major
+  ;; modes (e.g. org-mode) the font-lock-keywords is a buffer-local variable.
+  (if beancount-mode
+      (font-lock-add-keywords nil beancount-font-lock-defaults)
+    (font-lock-remove-keywords nil beancount-font-lock-defaults))
+  (font-lock-fontify-buffer)
+
+  (when beancount-mode
+    (make-variable-buffer-local 'beancount-accounts)
+    (beancount-init-accounts))
+  )
+
+
+(defun beancount-init-accounts ()
+  "Initialize or reset the list of accounts."
+  (interactive)
+  (setq beancount-accounts (beancount-get-accounts))
+  (message "Accounts updated."))
+
+
+(defvar beancount-directive-names '("txn"
+                                    "open"
+                                    "close"
+                                    "commodity"
+                                    "balance"
+                                    "pad"
+                                    "event"
+                                    "price"
+                                    "note"
+                                    "document"
+                                    "pushtag"
+                                    "poptag")
+  "A list of the directive names.")
+
+
+(defvar beancount-font-lock-defaults
+  `(;; Comments
+    (";.+" . font-lock-comment-face)
+
+    ;; Strings
+    ("\".*?\"" . font-lock-string-face)
+
+    ;; Reserved keywords
+    (,(regexp-opt beancount-directive-names) . font-lock-keyword-face)
+
+    ;; Tags & Links
+    ("[#\\^][A-Za-z0-9\-_/.]+" . font-lock-type-face)
+
+    ;; Date
+    ("[0-9][0-9][0-9][0-9][-/][0-9][0-9][-/][0-9][0-9]" . font-lock-constant-face)
+
+    ;; Account
+    ("\\([A-Z][A-Za-z0-9\-]+:\\)+\\([A-Z][A-Za-z0-9\-]+\\)" . font-lock-builtin-face)
+
+    ;; Txn Flags
+    ("! " . font-lock-warning-face)
+
+    ;; Number + Currency
+    ;;; ("\\([\\-+]?[0-9,]+\\(\\.[0-9]+\\)?\\)\\s-+\\([A-Z][A-Z0-9'\.]\\{1,10\\}\\)" . )
+    ))
+
+
+(defvar beancount-date-regexp "[0-9][0-9][0-9][0-9][-/][0-9][0-9][-/][0-9][0-9]"
+  "A regular expression to match dates.")
+
+(defvar beancount-account-regexp (concat (regexp-opt '("Assets"
+                                                       "Liabilities"
+                                                       "Equity"
+                                                       "Income"
+                                                       "Expenses"))
+                                         "\\(?::[A-Z][A-Za-z0-9-_:]+\\)")
+  "A regular expression to match account names.")
+
+(defvar beancount-number-regexp "[-+]?[0-9,\\.]+"
+  "A regular expression to match decimal numbers in beancount.")
+
+(defvar beancount-currency-regexp "[A-Z][A-Z-_'.]*"
+  "A regular expression to match currencies in beancount.")
+
+
+(defvar beancount-accounts nil
+  "A list of the accounts available in this buffer. This is a
+  cache of the value computed by beancount-get-accounts.")
+
+
+(defun beancount-hash-keys (hashtable)
+  "Extract all the keys of the given hashtable. Return a sorted list."
+  (let (rlist)
+    (maphash (lambda (k v) (push k rlist)) hashtable)
+    rlist))
+
+
+(defun beancount-get-accounts ()
+  "Heuristically obtain a list of all the accounts used in all the postings.
+We ignore patterns seen the line 'exclude-line'. If ALL is non-nil, look
+for account names in postings as well (default is to look at the @defaccount
+declarations only."
+  (let ((accounts (make-hash-table :test 'equal)))
+    (save-excursion
+      (goto-char (point-min))
+      (while (re-search-forward beancount-account-regexp nil t)
+        (puthash (match-string-no-properties 0) nil accounts)))
+    (sort (beancount-hash-keys accounts) 'string<)))
+
+
+(defun beancount-insert-account (account-name)
+  "Insert one of the valid account names in this file (using ido
+niceness)."
+  (interactive
+   (list (ido-completing-read "Account: " beancount-accounts nil nil (thing-at-point 'word))))
+  (let ((bounds (bounds-of-thing-at-point 'word)))
+    (when bounds
+      (delete-region (car bounds) (cdr bounds))))
+  (insert account-name))
+
+
+(defun beancount-transaction-set-flag ()
+  (interactive)
+  (save-excursion
+    (backward-paragraph 1)
+    (forward-line 1)
+    (replace-string "!" "*" nil (line-beginning-position) (line-end-position))))
+
+
+(defmacro beancount-for-line-in-region (begin end &rest exprs)
+  "Iterate over each line in region until an empty line is encountered."
+  `(save-excursion
+     (let ((end-marker (set-marker (make-marker) ,end)))
+       (goto-char ,begin)
+       (beginning-of-line)
+       (while (and (not (= (point) (point-max))) (< (point) end-marker))
+         (beginning-of-line)
+         (progn ,@exprs)
+         (forward-line 1)
+         ))))
+
+
+(defun beancount-max-accounts-width (begin end)
+  "Return the minimum widths of a list of account names on a list
+of lines. Initial whitespace is ignored."
+  (let* (widths)
+    (beancount-for-line-in-region
+     begin end
+     (let* ((line (thing-at-point 'line)))
+       (when (string-match
+              (concat "^[ \t]*\\(" beancount-account-regexp "\\)") line)
+         (push (length (match-string 1 line)) widths))))
+    (apply 'max widths)))
+
+
+(defun beancount-align-numbers (begin end &optional requested-currency-column)
+  "Align all numbers in the given region. CURRENCY-COLUMN is the character
+at which to align the beginning of the amount's currency. If not specified, use
+the smallest columns that will align all the numbers.  With a prefix argument,
+align with the fill-column."
+  (interactive "r")
+
+  ;; With a prefix argument, align with the fill-column.
+  (when current-prefix-arg
+    (setq requested-currency-column fill-column))
+
+  ;; Loop once in the region to find the length of the longest string before the
+  ;; number.
+  (let (prefix-widths
+        number-widths
+        (number-padding "  "))
+    (beancount-for-line-in-region
+     begin end
+     (let ((line (thing-at-point 'line)))
+       (when (string-match (concat "\\(.*?\\)"
+                                   "[ \t]+"
+                                   "\\(" beancount-number-regexp "\\)"
+                                   "[ \t]+"
+                                   beancount-currency-regexp) line)
+         (push (length (match-string 1 line)) prefix-widths)
+         (push (length (match-string 2 line)) number-widths)
+         )))
+
+    (when prefix-widths
+      ;; Loop again to make the adjustments to the numbers.
+      (let* ((number-width (apply 'max number-widths))
+             (number-format (format "%%%ss" number-width))
+             ;; Compute rightmost column of prefix.
+             (max-prefix-width (apply 'max prefix-widths))
+             (max-prefix-width
+              (if requested-currency-column
+                  (max (- requested-currency-column (length number-padding) number-width 1)
+                       max-prefix-width)
+                max-prefix-width))
+             (prefix-format (format "%%-%ss" max-prefix-width))
+             )
+
+        (beancount-for-line-in-region
+         begin end
+         (let ((line (thing-at-point 'line)))
+           (when (string-match (concat "^\\([^\"]*?\\)"
+                                       "[ \t]+"
+                                       "\\(" beancount-number-regexp "\\)"
+                                       "[ \t]+"
+                                       "\\(.*\\)$") line)
+             (delete-region (line-beginning-position) (line-end-position))
+             (let* ((prefix (match-string 1 line))
+                    (number (match-string 2 line))
+                    (rest (match-string 3 line)) )
+               (insert (format prefix-format prefix))
+               (insert number-padding)
+               (insert (format number-format number))
+               (insert " ")
+               (insert rest)))))))))
+
+
+(defun beancount-align-to-previous-number ()
+  "Align postings under the point's paragraph.
+This function looks for a posting in the previous transaction to
+determine the column at which to align the transaction, or otherwise
+the fill column, and align all the postings of this transaction to
+this column."
+  (interactive)
+  (let* ((begin (save-excursion
+                  (beancount-beginning-of-directive)
+                  (point)))
+         (end (save-excursion
+                (goto-char begin)
+                (forward-paragraph 1)
+                (point)))
+         (currency-column (or (beancount-find-previous-alignment-column)
+                              fill-column)))
+    (beancount-align-numbers begin end currency-column)))
+
+
+(defun beancount-beginning-of-directive ()
+  "Move point to the beginning of the enclosed or preceding directive."
+  (beginning-of-line)
+  (while (and (> (point) (point-min))
+              (not (looking-at
+                      "[0-9][0-9][0-9][0-9][\-/][0-9][0-9][\-/][0-9][0-9]")))
+    (forward-line -1)))
+
+
+(defun beancount-find-previous-alignment-column ()
+  "Find the preceding column to align amounts with.
+This is used to align transactions at the same column as that of
+the previous transaction in the file. This function merely finds
+what that column is and returns it (an integer)."
+  ;; Go hunting for the last column with a suitable posting.
+  (let (column)
+    (save-excursion
+      ;; Go to the beginning of the enclosing directive.
+      (beancount-beginning-of-directive)
+      (forward-line -1)
+
+      ;; Find the last posting with an amount and a currency on it.
+      (let ((posting-regexp (concat
+                             "\\s-+"
+                             beancount-account-regexp "\\s-+"
+                             beancount-number-regexp "\\s-+"
+                             "\\(" beancount-currency-regexp "\\)"))
+            (balance-regexp (concat
+                             beancount-date-regexp "\\s-+"
+                             "balance" "\\s-+"
+                             beancount-account-regexp "\\s-+"
+                             beancount-number-regexp "\\s-+"
+                             "\\(" beancount-currency-regexp "\\)")))
+        (while (and (> (point) (point-min))
+                    (not (or (looking-at posting-regexp)
+                             (looking-at balance-regexp))))
+          (forward-line -1))
+        (when (or (looking-at posting-regexp)
+                  (looking-at balance-regexp))
+          (setq column (- (match-beginning 1) (point))))
+        ))
+    column))
+
+
+(defvar beancount-check-program "bean-check"
+  "Program to run to run just the parser and validator on an
+  input file.")
+
+(defun beancount-check ()
+  (interactive)
+  (let ((compilation-read-command nil)
+        (compile-command
+         (format "%s %s" beancount-check-program (buffer-file-name))))
+    (call-interactively 'compile)))
+
+
+(defvar beancount-query-program "bean-query"
+  "Program to run to run just the parser and validator on an
+  input file.")
+
+(defun beancount-query ()
+  (interactive)
+  (let ((compile-command
+         (format "%s %s " beancount-query-program (buffer-file-name))))
+    (call-interactively 'compile)))
+
+
+(defvar beancount-doctor-program "bean-doctor"
+  "Program to run the doctor commands.")
+
+(defun beancount-context ()
+  (interactive)
+  (let ((compilation-read-command nil)
+        (compile-command
+         (format "%s %s %s %d"
+                 beancount-doctor-program "context"
+                 (buffer-file-name) (line-number-at-pos (point)))))
+    (call-interactively 'compile)))
+
+
+(provide 'beancount)
---
+++ init.el
@@ -0,0 +1,271 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+;; don't use this but use-package
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+
+;; see use-package syntax below
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+;; from Aaron Bieber's config
+(use-package helm
+  :ensure t
+  :diminish helm-mode
+  :commands helm-mode
+  :config
+  (helm-mode 1)
+  (setq helm-buffers-fuzzy-matching t)
+  (setq helm-autoresize-mode t)
+  (setq helm-buffer-max-length 40)
+  (define-key helm-map (kbd "S-SPC") 'helm-toggle-visible-mark)
+  (define-key helm-find-files-map (kbd "C-k") 'helm-find-files-up-one-level))
+
+;; from Aaron Bieber's config
+(use-package company
+  :ensure t
+  :defer t
+  :init
+  (global-company-mode)
+  :config
+
+  (defun org-keyword-backend (command &optional arg &rest ignored)
+    "Company backend for org keywords.
+COMMAND, ARG, IGNORED are the arguments required by the variable
+`company-backends', which see."
+    (interactive (list 'interactive))
+    (cl-case command
+      (interactive (company-begin-backend 'org-keyword-backend))
+      (prefix (and (eq major-mode 'org-mode)
+                   (let ((p (company-grab-line "^#\\+\\(\\w*\\)" 1)))
+                     (if p (cons p t)))))
+      (candidates (mapcar #'upcase
+                          (cl-remove-if-not
+                           (lambda (c) (string-prefix-p arg c))
+                           (pcomplete-completions))))
+      (ignore-case t)
+      (duplicates t)))
+  (add-to-list 'company-backends 'org-keyword-backend)
+
+  (setq company-idle-delay 0.4)
+  (setq company-selection-wrap-around t)
+  (define-key company-active-map (kbd "ESC") 'company-abort)
+  (define-key company-active-map [tab] 'company-complete-common-or-cycle)
+  (define-key company-active-map (kbd "C-n") 'company-select-next)
+  (define-key company-active-map (kbd "C-p") 'company-select-previous))
+
+;; Mac keys (italian layout)
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier nil)
+
+;; Capture templates for: TODO tasks, Notes, appointments, phone calls, meetings, and org-protocol
+;; %a          annotation, normally the link created with org-store-link
+;; %i          initial content, the region when capture is called with C-u.
+;; %t, %T      timestamp, date only, or date and time
+;; %u, %U      like above, but inactive timestamps
+;; %?          After completing the template, position cursor here.
+
+(setq org-capture-templates
+      (quote (("t" "todo" entry (file+headline "~/Dropbox/cloudOrg/orgmode.org" "GTD")
+               "* TODO %?\n%U\n")
+              ("r" "reply" entry (file+headline "~/Dropbox/cloudOrg/orgmode.org" "GTD")
+               "* NEXT Mail reply to %:from on %:subject\nEntered on %U\nSCHEDULED: %^t")
+              ("n" "note" entry (file+headline "~/Dropbox/cloudOrg/orgmode.org" "Notes")
+               "* %? :NOTE:\nEntered on %U\n")
+              ("j" "Journal" entry (file+olp+datetree "~/Dropbox/cloudOrg/diary.org")
+               "* %?\nEntered on %U\n")
+              ("m" "Meeting" entry (file+headline "~/Dropbox/cloudOrg/orgmode.org" "GTD")
+               "* MEETING with %? :MEETING:\nEntered on %U\nSCHEDULED: %^T")
+              ("p" "Phone call" entry (file+headline "~/Dropbox/cloudOrg/orgmode.org" "GTD")
+               "* PHONE %? :PHONE:\nEntered on %U\n")
+	      ("w" "org-protocol" entry (file "~/Dropbox/cloudOrg/refile.org")
+               "* TODO Review %c\nEntered on %U\n")
+	      ("c" "Rescue mission" entry (file+headline "~/Dropbox/cloudOrg/CRI.org" "Missions") "* Mission %^{CODE}p  %^{DIAGNOSIS}p %^{BRIEFING}p\n Entered on %T\n")
+              ("h" "Habit" entry (file+headline "~/Dropbox/cloudOrg/orgmode.org" "Habits")
+               "* NEXT %?\nEntered on %U\nSCHEDULED: %(format-time-string \"%<<%Y-%m-%d %a .+1d/3d>>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n"))))
+
+;; ido mode
+(ido-mode)
+(setq org-completion-use-ido t)
+
+;; Set location for external custom packages.
+(add-to-list 'load-path "~/.emacs.d/custom/")
+
+;; Set which files must be visible on agenda
+(setq org-agenda-files (list "~/Dropbox/cloudOrg/orgmode.org"
+			     "~/Dropbox/cloudOrg/francis_gtd.org"))
+
+;; config necessary to org-mode
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
+;; agenda week starts at present day
+(setq org-agenda-start-on-weekday nil)
+
+;; if you want headlines indented
+;;(setq org-startup-indented t)
+
+;; for nice bullets - commented
+;; (require 'org-bullets)
+;; (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
+;; beancount
+(require 'beancount)
+
+;; gnupg
+;; (require 'epa-file)
+;; (setq epg-gpg-program "/usr/local/Cellar/gnupg@2.1")
+;; (epa-file-enable)
+
+;; evil-mode
+(add-to-list 'load-path "~/.emacs.d/evil")
+(require 'evil)
+(evil-mode 1)
+
+;; theme
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(load-theme 'zenburn t)
+
+;; ;; mu4e
+(add-to-list 'load-path "/usr/local/Cellar/mu/1.0/share/emacs/site-lisp/mu/mu4e/")
+(require 'mu4e)
+
+(setq mu4e-maildir "~/Maildir")
+; (setq mu4e-drafts-folder "/[Gmail].Drafts")
+(setq mu4e-sent-folder   "/[Gmail].Sent Mail")
+;; don't save message to Sent Messages, Gmail/IMAP takes care of this
+(setq mu4e-sent-messages-behavior 'delete)
+;; allow for updating mail using 'U' in the main view:
+(setq mu4e-get-mail-command "offlineimap")
+
+;; shortcuts
+(setq mu4e-maildir-shortcuts
+    '( ("/INBOX"               . ?i)
+       ("/[Gmail].Sent Mail"   . ?s)))
+
+;; something about ourselves
+(setq
+   user-mail-address "longhi.francesco@gmail.com"
+   user-full-name  "Francesco Longhi"
+   mu4e-compose-signature
+    (concat
+      "Saluti,\n"
+      "Francesco Longhi\n"))
+
+;; show images
+(setq mu4e-show-images t)
+
+;; use imagemagick, if available
+(when (fboundp 'imagemagick-register-types)
+  (imagemagick-register-types))
+
+;; convert html emails properly
+;; Possible options:
+;;   - html2text -utf8 -width 72
+;;   - textutil -stdin -format html -convert txt -stdout
+;;   - html2markdown | grep -v '&nbsp_place_holder;' (Requires html2text pypi)
+;;   - w3m -dump -cols 80 -T text/html
+;;   - view in browser (provided below)
+(setq mu4e-html2text-command "textutil -stdin -format html -convert txt -stdout")
+
+;; spell check
+(add-hook 'mu4e-compose-mode-hook
+        (defun my-do-compose-stuff ()
+           "My settings for message composition."
+           (set-fill-column 72)
+           (flyspell-mode)))
+
+;; add option to view html message in a browser
+;; `aV` in view to activate
+;; (add-to-list 'mu4e-view-actions
+;;   '("ViewInBrowser" . mu4e-action-view-in-browser) t)
+
+;; fetch mail every 10 mins
+(setq mu4e-update-interval 600)
+
+;; xelatex
+(eval-after-load "tex"
+    '(add-to-list 'TeX-command-list
+                  '("xelatex" "xelatex -interaction=nonstopmode %s"
+                    TeX-run-command t t :help "Run xelatex") t))
+
+;; koma-letter
+(add-to-list 'auto-mode-alist '("\\.lco" . tex-mode))
+(eval-after-load 'ox '(require 'ox-koma-letter))
+
+(eval-after-load 'ox-latex
+  '(add-to-list 'org-latex-packages-alist '("AUTO" "babel" t) t))
+
+  ;; babel
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((emacs-lisp . t)
+     (lisp . t)
+     (python . t)
+     (R . t)
+     (shell . t)))
+
+;; logging into drawers
+(setq org-log-done (quote time))
+(setq org-log-into-drawer t)
+(setq org-log-redeadline (quote note));; record when the deadline date of a tasks is modified
+(setq org-log-reschedule (quote time))
+
+;; refile targets
+(setq org-refile-targets (quote ((org-agenda-files :level . 1))))
+
+;; Flyspell, aspell
+;; you must enable with M-x flyspell-mode
+;; change default dictionary (en) with "ispell-change-dictionary"
+;; auto correct associated to C-M-i
+(require 'flyspell)
+(flyspell-mode +1)
+
+(add-hook 'LaTeX-mode-hook 'flyspell-mode) ;start flyspell-mode
+(setq ispell-dictionary "italian")    ;set the default dictionary
+(add-hook 'LaTeX-mode-hook 'ispell)   ;start ispell
+
+;; yasnippet
+(add-to-list 'load-path
+              "~/.emacs.d/elpa/yasnippet-0.13.0")
+(require 'yasnippet)
+(yas-global-mode 1)
+
+;; org-edna
+(add-to-list 'load-path "~/.emacs.d/custom/org-edna-1.0beta6")
+
+(org-edna-load)
+
+(setq make-backup-files nil) ; stop creating backup~ files
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (org-edna yasnippet zenburn-theme use-package helm company))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
